133e991b95178d34c507ef30488aa97a7585048c
Juan Villacorta <juanvill2204@gmail.com>
2026-05-02 22:39:46 -0700

    Update: improve the implement core backend API routes

--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -1,20 +1,12 @@
 const express = require("express");
-const departments = require("../data/departments.json");
+const {
+  getAllDepartments,
+  getDepartmentById
+} = require("../controllers/departments.controller");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(departments);
-});
-
-router.get("/:id", (req, res) => {
-  const department = departments.find((item) => item.id === req.params.id);
-
-  if (!department) {
-    return res.status(404).json({ message: "Department not found" });
-  }
-
-  res.json(department);
-});
+router.get("/", getAllDepartments);
+router.get("/:id", getDepartmentById);
 
 module.exports = router;
