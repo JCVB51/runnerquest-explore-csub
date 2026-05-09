e34efc54aee2187821c6256d588614dbac1a14d8
douglasC <douglascerrato2@gmail.com>
2026-05-09 15:50:57 -0700

    created library scene template

--- runner explorer/assets/Room_Builder_free_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Room_Builder_free_16x16" tilewidth="16" tileheight="16" tilecount="391" columns="17">
- <image source="Modern tiles_Free 16x16/Interiors_free/16x16/Room_Builder_free_16x16.png" width="272" height="368"/>
-</tileset>
