2d9eff6705ce1bbd2ec807658f8d9a5db6da70f0
Joseph Hernandez <josephh1234abcd@gmail.com>
2026-05-10 21:40:11 -0700

    Added tsx folder for tilesets

---
+++ runner explorer/assets/tsx/Bob_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Bob_16x16" tilewidth="16" tileheight="16" tilecount="336" columns="24">
+ <image source="../../../../Downloads/Modern_Interiors_Free_v2.2/Modern tiles_Free/Characters_free/Bob_16x16.png" width="384" height="224"/>
+</tileset>
